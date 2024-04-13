07283ed5f151f4e08a0163e9c13139ee92404e7c
NGrinstead0101 <ngrinstead@mail.bradley.edu>
2024-04-13 14:00:23 -0500

    Collision Updates

---
+++ Assets/Scripts/Player/UpdatePlayerLayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UpdatePlayerLayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        int numPlayers = PlayerInputManager.instance.playerCount;
+        if (numPlayers == 2)
+        {
+            tag = "Player1";
+            gameObject.layer = 6;
+
+            foreach (Transform child in transform)
+            {
+                child.tag = "Player1";
+                child.gameObject.layer = 6;
+            }
+        }
+        else
+        {
+            tag = "Player2";
+            gameObject.layer = 7;
+
+            foreach (Transform child in transform)
+            {
+                child.tag = "Player2";
+                child.gameObject.layer = 7;
+            }
+        }
+    }
+}
