188b4c0ebf0281e397e3505426ec8ce7d74153df
Caden-Sheahan <sheahancj@outlook.com>
2024-04-17 11:48:56 -0500

    Fixed this properly

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -5,8 +5,18 @@ using UnityEngine.EventSystems;
 
 public class ButtonController : MonoBehaviour
 {
+
+
     private void OnEnable()
     {
+        StartCoroutine(SetSelectedButton());
+    }
+
+    private IEnumerator SetSelectedButton()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
         EventSystem.current.firstSelectedGameObject = transform.GetChild(0).gameObject;
+        EventSystem.current.SetSelectedGameObject(transform.GetChild(0).gameObject); 
     }
 }
