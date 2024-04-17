9249d8f88863a0be9cdc4d9578ac6dad7af78915
AndreaXyna <andreaswihartdecoster@gmail.com>
2024-04-16 21:27:07 -0500

    Events stop on obj destroy

--- Assets/Scripts/AmbientController.cs
+++ Assets/Scripts/AmbientController.cs
@@ -8,7 +8,7 @@ public class AmbientController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        if (Instance == null)
+        /*if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(this);
@@ -16,6 +16,6 @@ public class AmbientController : MonoBehaviour
         else
         {
             Destroy(this);
-        }
+        }*/
     }
 }
