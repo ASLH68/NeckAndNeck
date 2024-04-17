bdb615922bdf15ae9231d17649e739bfa029cc57
AndreaXyna <andreaswihartdecoster@gmail.com>
2024-04-16 21:16:32 -0500

    Multiple music instances big fix

--- Assets/Scripts/WinTrigger.cs
+++ Assets/Scripts/WinTrigger.cs
@@ -24,11 +24,11 @@ public class WinTrigger : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player1"))
+        if (other.transform.parent.CompareTag("Player1"))
         {
             OnPlayerDied?.Invoke(1);
         }
-        else if (other.CompareTag("Player2"))
+        else if (other.transform.parent.CompareTag("Player2"))
         {
             OnPlayerDied?.Invoke(2);
         }
